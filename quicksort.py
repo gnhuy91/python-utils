da62b20f3653a274063954d0976bef549c5f7168
Huy Giang <gnhuy91@gmail.com>
2016-07-23 21:21:12 +0700

    add new & correct func

--- quicksort.py
+++ quicksort.py
@@ -29,7 +29,30 @@ def quick_sort(arr, l, r):
             quick_sort(arr, i, r)
 
 
+def partition(array, begin, end):
+    pivot = begin
+    for i in xrange(begin + 1, end + 1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+
+    if begin >= end:
+        return
+
+    pivot = partition(array, begin, end)
+    quicksort(array, begin, pivot - 1)
+    quicksort(array, pivot + 1, end)
+
+
 if __name__ == '__main__':
-    arr = [12, 4, 5, 6, 7, 3, 1, 15]
-    quick_sort(arr, 0, len(arr) - 1)
+    arr = [12, 2, 8, 5, 1, 6, 4, 15]
+    quicksort(arr, 0, len(arr) - 1)
     print arr
