35367de7a727926db72d480ed408bc41c5fca5ac
Huy Giang <gnhuy91@gmail.com>
2016-07-23 19:54:21 +0700

    remove unnecessary else

--- quicksort.py
+++ quicksort.py
@@ -10,26 +10,26 @@ def quick_sort(arr, l, r):
 
     if len(arr) == 0:
         return arr
-    else:
-        while True:
-            while arr[i] < x:
-                i += 1
-            while arr[j] > x:
-                j -= 1
-            if i <= j:
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
-                i += 1
-                j -= 1
 
-            if i >= j:
-                break
+    while True:
+        while arr[i] < x:
+            i += 1
+        while arr[j] > x:
+            j -= 1
+        if i <= j:
+            tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+            i += 1
+            j -= 1
 
-            if l < j:
-                quick_sort(arr, l, j)
-            if i < r:
-                quick_sort(arr, i, r)
+        if i >= j:
+            break
+
+        if l < j:
+            quick_sort(arr, l, j)
+        if i < r:
+            quick_sort(arr, i, r)
 
 
 if __name__ == '__main__':
